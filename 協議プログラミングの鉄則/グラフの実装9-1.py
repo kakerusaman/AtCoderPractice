0a7cbb994c218fcc635ecb9e6feeb5e294d56f8d
k-itaba <k-itaba@newral.co.jp>
2025-04-21 07:42:50 +0900

    グラフの実装を解きました。

---
+++ 協議プログラミングの鉄則/グラフの実装9-1.py
@@ -0,0 +1,20 @@
+N,M = map(int,input().split())
+
+edges = [list(map(int,input().split())) for _ in range(M)]
+
+G = [list() for i in range(N+1)]
+
+for a,b in edges:
+    G[a].append(b)
+    G[b].append(a)
+
+
+N,M = map(int,input().split())
+
+edges = [list(map(int,input().split())) for _ in range(N+1)]
+
+G = [list() for _ in range(N+1)]
+
+for a,b in edges:
+    G[a].append(b)
+    G[b].append(a)
